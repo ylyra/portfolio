6c8790794216c86fe8b93f7a7626e36754c69f7a
Yan Lyra <yanlyra3@gmail.com>
2022-04-04 19:54:12 -0300

    🐛 Fixed bugs

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { ChangeEvent, FormEvent, useState } from "react";
 import InputMask from "react-input-mask";
 
@@ -6,46 +7,6 @@ import api from "../services/api";
 import styles from "../styles/components/Contact.module.css";
 
 export function Contact() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [message, setMessage] = useState("");
-  const [submitting, setSubmitting] = useState(false);
-  const [submitted, setSubmitted] = useState(false);
-  const [submittedMessage, setSubmittedMessage] = useState("");
-
-  async function handleContact(event: FormEvent) {
-    event.preventDefault();
-
-    setSubmitting(true);
-
-    const data = new FormData();
-    data.append("name", name);
-    data.append("email", email);
-    data.append("phone", phone);
-    data.append("message", message);
-
-    const response = await api.post(
-      "https://yanlyraportfolio.000webhostapp.com/ajax/contact",
-      data
-    );
-
-    if (response.status === 200) {
-      setSubmitting(false);
-      setSubmittedMessage(response.data.status);
-      setSubmitted(true);
-    }
-
-    resetContentForm();
-  }
-
-  function resetContentForm() {
-    setName("");
-    setEmail("");
-    setPhone("");
-    setMessage("");
-  }
-
   return (
     <section id="contact" className={styles.contactContainer}>
       <div className="container">
@@ -58,90 +19,91 @@ export function Contact() {
               urgente use o link para o WhatsApp ao lado.
             </p>
 
-            {!submitted ? (
-              <form method="post" onSubmit={handleContact}>
-                <div className={styles.formGroup}>
-                  <label htmlFor="name">Seu Nome (Obrigatório)</label>
-                  <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    placeholder="Digite o seu nome"
-                    className="form-control"
-                    value={name}
-                    onChange={(event) => setName(event.target.value)}
-                    required
-                  />
-                </div>
-
-                <div className={styles.formGroup}>
-                  <label htmlFor="email">Seu E-mail (Obrigatório)</label>
-                  <input
-                    type="text"
-                    name="email"
-                    id="email"
-                    placeholder="Digite o seu nome (exemplo: email@gmail.com)"
-                    className="form-control"
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}
-                    required
-                  />
-                </div>
-
-                <div className={styles.formGroup}>
-                  <label htmlFor="phone_number">
-                    Número do celular (Opcional)
-                  </label>
-                  <InputMask
-                    mask="+55 (99) 9-9999-9999"
-                    type="text"
-                    id="phone_number"
-                    className="form-control"
-                    value={phone}
-                    placeholder="Digite o telefone para contato por WhatsApp"
-                    onChange={(event: ChangeEvent<HTMLInputElement>) =>
-                      setPhone(event.target.value)
-                    }
-                  />
-                </div>
-
-                <div className={styles.formGroup}>
-                  <label htmlFor="message">Mensagem (Obrigatório)</label>
-                  <textarea
-                    name="message"
-                    id="message"
-                    className="form-control"
-                    placeholder="Deixe sua mensagem de contato aqui"
-                    value={message}
-                    onChange={(event) => setMessage(event.target.value)}
-                    required
-                  ></textarea>
-                </div>
-
-                <div className={styles.formButtons}>
-                  {!submitting ? (
-                    <button
-                      type="submit"
-                      className={`btn ${styles.btn} ${styles.green}`}
-                    >
-                      Enviar
-                    </button>
-                  ) : (
-                    <button
-                      type="button"
-                      disabled
-                      className={`btn ${styles.btn} ${styles.sending}`}
-                    >
-                      Enviando...
-                    </button>
-                  )}
-                </div>
-              </form>
-            ) : (
-              <div className={styles.contactFormMessage}>
-                {submittedMessage}
+            <form
+              action="https://formsubmit.co/yanlyra3@gmail.com"
+              method="POST"
+            >
+              <div className={styles.formGroup}>
+                <label htmlFor="name">Seu Nome (Obrigatório)</label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Digite o seu nome"
+                  className="form-control"
+                  defaultValue=""
+                  required
+                />
               </div>
-            )}
+
+              <div className={styles.formGroup}>
+                <label htmlFor="email">Seu E-mail (Obrigatório)</label>
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  placeholder="Digite o seu nome (exemplo: email@gmail.com)"
+                  className="form-control"
+                  defaultValue=""
+                  required
+                />
+              </div>
+
+              <div className={styles.formGroup}>
+                <label htmlFor="phone_number">
+                  Número do celular (Opcional)
+                </label>
+                <InputMask
+                  mask="+55 (99) 9-9999-9999"
+                  type="text"
+                  id="phone_number"
+                  className="form-control"
+                  placeholder="Digite o telefone para contato por WhatsApp"
+                  defaultValue=""
+                />
+              </div>
+
+              <div className={styles.formGroup}>
+                <label htmlFor="message">Mensagem (Obrigatório)</label>
+                <textarea
+                  name="message"
+                  id="message"
+                  className="form-control"
+                  placeholder="Deixe sua mensagem de contato aqui"
+                  defaultValue=""
+                  required
+                ></textarea>
+              </div>
+
+              <input
+                type="hidden"
+                name="_next"
+                defaultValue="https://yanlyra.dev"
+              />
+              <input
+                type="hidden"
+                name="_subject"
+                defaultValue="Contato pelo site!"
+              />
+              <input
+                type="text"
+                name="_honey"
+                defaultValue=""
+                style={{
+                  display: "none",
+                }}
+              />
+              <input type="hidden" name="_captcha" defaultValue="true" />
+
+              <div className={styles.formButtons}>
+                <button
+                  type="submit"
+                  className={`btn ${styles.btn} ${styles.green}`}
+                >
+                  Enviar
+                </button>
+              </div>
+            </form>
           </div>
 
           <div className={`col s12 m6 ${styles.asc}`}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,37 @@
-import Head from 'next/head'
-import { About } from '../components/About'
-import { Contact } from '../components/Contact'
-import { Footer } from '../components/Footer'
-import { NavBar } from '../components/NavBar'
-import { TopContent } from '../components/TopContent'
-import { Works } from '../components/Works'
-import { WorksProvider, WorkData } from '../contexts/WorksContext'
-import api from '../services/api'
-
+import axios from "axios";
+import Head from "next/head";
+import { About } from "../components/About";
+import { Contact } from "../components/Contact";
+import { Footer } from "../components/Footer";
+import { NavBar } from "../components/NavBar";
+import { TopContent } from "../components/TopContent";
+import { Works } from "../components/Works";
+import { WorksProvider, WorkData } from "../contexts/WorksContext";
+import api from "../services/api";
 
 interface PropsData {
   allWorks: WorkData[];
 }
 
 export default function Home(props: PropsData) {
-  
   return (
     <div>
       <Head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <title>Início | Yan</title>
-        <meta name="description" content="Bem vindo ao meu portfolio, aqui você irá encontrar todos os trabalhos que eu fiz." />
+        <meta
+          name="description"
+          content="Bem vindo ao meu portfolio, aqui você irá encontrar todos os trabalhos que eu fiz."
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         <meta property="og:site_name" content="Início | Yan" />
         <meta property="og:title" content="Início | Yan" />
-        <meta property="og:description" content="Bem vindo ao meu portfolio, aqui você irá encontrar todos os trabalhos que eu fiz." />
+        <meta
+          property="og:description"
+          content="Bem vindo ao meu portfolio, aqui você irá encontrar todos os trabalhos que eu fiz."
+        />
         <meta property="og:url" content="https://yanlyra.com.br" />
         <meta property="og:locale" content="pt_BR" />
         <meta property="og:image" content="./apple-icon-180x180.png" />
@@ -38,32 +43,34 @@ export default function Home(props: PropsData) {
         <meta name="twitter:site" content="@ylyra1" />
         <meta name="twitter:creator" content="@ylyra1" />
       </Head>
-      
-      <NavBar /> 
+
+      <NavBar />
       <WorksProvider works={props.allWorks}>
-        <TopContent />   
-        
+        <TopContent />
+
         <About />
 
         <Works />
       </WorksProvider>
 
       <Contact />
-      
 
       <Footer />
     </div>
-  )
+  );
 }
 
 export async function getStaticProps() {
-  const { data } = await api.get('/api/trabalhos')
-  const allWorks = data;
-  
+  let allWorks = [];
+  try {
+    const { data } = await axios.get("/api/trabalhos");
+    allWorks = data;
+  } catch {}
+
   return {
     props: {
-      allWorks
+      allWorks,
     }, // will be passed to the page component as props
-    revalidate: 86400
-  }
+    revalidate: 86400,
+  };
 }
