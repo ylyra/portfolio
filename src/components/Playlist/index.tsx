a177de899cd45346deceafaf84539681ad1aba72
Yan Lyra <yanlyra3@gmail.com>
2023-07-20 14:55:35 -0300

    chore: fixed lints

--- src/components/Playlist/index.tsx
+++ src/components/Playlist/index.tsx
@@ -1,14 +1,20 @@
 import { styled } from '@/panda/jsx'
 
-
 export const Playlist = () => {
   return (
     <styled.section mt="2rem" width="100%">
-      <styled.h2 fontWeight="700" fontSize={'1rem'} px="0.5rem" mb="24">
+      <styled.h2 fontWeight="700" fontSize={'1rem'} px="0.5rem" mb="0.75rem">
         Songs I can&apos;t stop listening to
       </styled.h2>
 
-      <iframe src="https://open.spotify.com/embed/playlist/7myJpCbfX71JZiDQembjiz?utm_source=generator&theme=0" width="100%" height="352" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+      <iframe
+        src="https://open.spotify.com/embed/playlist/7myJpCbfX71JZiDQembjiz?utm_source=generator&theme=0"
+        width="100%"
+        height="352"
+        allowFullScreen
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      />
     </styled.section>
   )
 }
